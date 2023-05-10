5c08c96d0334304984382437e39d81d5f53c575b
juancamilovargasmoreno <juancamilovargasmoreno46@gmail.com>
2023-05-10 09:19:41 -0500

    Initial commit

--- Codigo 2023.py
+++ Codigo 2023.py
@@ -3,3 +3,7 @@ from typing import Tuple
 class DatosMeteorologicos:
     def __init__(self, nombre_archivo: str):
         self.nombre_archivo = nombre_archivo
+
+        def procesar_datos(self) -> Tuple[float, float, float, float, str]:
+            with open(self.nombre_archivo, "r") as f:
+                datos = f.readlines()
